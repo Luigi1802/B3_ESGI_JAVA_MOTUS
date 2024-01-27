b0e6aaaaeebc330326e5d33fae8b31bc38f3e95c
arthur-herve <98807046+arthur-herve@users.noreply.github.com>
2024-01-27 18:11:59 +0100

    feat: creation de la fonction d'import de mots, le filtrage des mots pourqu'il n'y ait que des lettres, tous les mots sont mis dans une seule liste

--- src/main/java/fr/esgi/App.java
+++ src/main/java/fr/esgi/App.java
@@ -6,7 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * JavaFX App
@@ -29,6 +38,33 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    private static Stream<String> retournerImportMot(String urlPath)  {
+        URL url = null;
+        Pattern pattern = Pattern.compile("[^a-zA-Z]");
+        try {
+            url = new URL(urlPath);
+        } catch (MalformedURLException e) {
+            System.out.println("Fichier teste indisponible, raison : " + e.getMessage());
+            System.exit(-1);
+        }
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((url.openStream())));
+            Stream<String> streamLines = bufferedReader.lines();
+
+            return  streamLines.filter(pattern.asPredicate().negate());
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+    private static ArrayList<String> creerListeMots(){
+        ArrayList<String> listeDesMots = new ArrayList<>();
+        listeDesMots.addAll(new ArrayList<String>(retournerImportMot("https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire6.txt").collect(Collectors.toList())));
+        listeDesMots.addAll(new ArrayList<String>(retournerImportMot("https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire7.txt").collect(Collectors.toList())));
+        listeDesMots.addAll(new ArrayList<String>(retournerImportMot("https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire8.txt").collect(Collectors.toList())));
+        return listeDesMots;
     }
 
     public static void main(String[] args) {
