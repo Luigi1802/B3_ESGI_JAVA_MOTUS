c344f51480ee56aa036023506bbce060b54c8efe
arthur-herve <98807046+arthur-herve@users.noreply.github.com>
2024-01-27 18:51:37 +0100

    feat: Finalisation import des mots depuis une URL en mettant les méthodes dans un service ImportMotsService et la classe ImportMotsServiceImpl

--- src/main/java/fr/esgi/App.java
+++ src/main/java/fr/esgi/App.java
@@ -1,5 +1,7 @@
 package fr.esgi;
 
+import fr.esgi.service.ImportMotsService;
+import fr.esgi.service.impl.ImportMotsServiceImpl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,6 +23,7 @@ import java.util.stream.Stream;
  * JavaFX App
  */
 public class App extends Application {
+    private static ImportMotsService importMotsService = new ImportMotsServiceImpl();
 
     private static Scene scene;
 
@@ -38,33 +41,6 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
-    }
-
-    private static Stream<String> retournerImportMot(String urlPath)  {
-        URL url = null;
-        Pattern pattern = Pattern.compile("[^a-zA-Z]");
-        try {
-            url = new URL(urlPath);
-        } catch (MalformedURLException e) {
-            System.out.println("Fichier teste indisponible, raison : " + e.getMessage());
-            System.exit(-1);
-        }
-        try{
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((url.openStream())));
-            Stream<String> streamLines = bufferedReader.lines();
-
-            return  streamLines.filter(pattern.asPredicate().negate());
-
-        }catch (IOException e){
-            throw new RuntimeException(e);
-        }
-    }
-    private static ArrayList<String> creerListeMots(){
-        ArrayList<String> listeDesMots = new ArrayList<>();
-        listeDesMots.addAll(new ArrayList<String>(retournerImportMot("https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire6.txt").collect(Collectors.toList())));
-        listeDesMots.addAll(new ArrayList<String>(retournerImportMot("https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire7.txt").collect(Collectors.toList())));
-        listeDesMots.addAll(new ArrayList<String>(retournerImportMot("https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire8.txt").collect(Collectors.toList())));
-        return listeDesMots;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/fr/esgi/service/impl/ImportMotsServiceImpl.java
@@ -0,0 +1,54 @@
+package fr.esgi.service.impl;
+
+import fr.esgi.service.ImportMotsService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ImportMotsServiceImpl implements ImportMotsService {
+    private final String sixLettresURL = "https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire6.txt";
+    private final String septLettresURL = "https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire7.txt";
+    private final String huitLettresURL = "https://raw.githubusercontent.com/gverdier/motus/master/Console/Dictionnaire8.txt";
+    private static ArrayList<String> listeMots = new ArrayList<>();
+    @Override
+    public  Stream<String> retournerImportMot(String urlPath)  {
+        URL url = null;
+        Pattern pattern = Pattern.compile("[^a-zA-Z]");
+        try {
+            url = new URL(urlPath);
+        } catch (MalformedURLException e) {
+            System.out.println("Fichier teste indisponible, raison : " + e.getMessage());
+            System.exit(-1);
+        }
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((url.openStream())));
+            Stream<String> streamLines = bufferedReader.lines();
+
+            return  streamLines.filter(pattern.asPredicate().negate());
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public ArrayList<String> transformerStreamStringEnListe(Stream<String> streamATransformer){
+        return streamATransformer.collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Override
+    public void creerListeMots(){
+        listeMots.addAll(transformerStreamStringEnListe(retournerImportMot(sixLettresURL)));
+        listeMots.addAll(transformerStreamStringEnListe(retournerImportMot(septLettresURL)));
+        listeMots.addAll(transformerStreamStringEnListe(retournerImportMot(huitLettresURL)));
+    }
+    @Override
+    public ArrayList<String> recupererListeMots(){return listeMots;};
+}
