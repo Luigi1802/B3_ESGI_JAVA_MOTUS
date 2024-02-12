9d7c667341696468d858f2419ab6c067a2e1e3a4
arthur-herve <98807046+arthur-herve@users.noreply.github.com>
2024-02-12 07:12:20 +0100

    Commentaire des classes métier Lettre, Mot et de l'énumération StatutLettre dans un format pour une javadoc

--- src/main/java/fr/esgi/business/StatutLettre.java
+++ src/main/java/fr/esgi/business/StatutLettre.java
@@ -1,8 +1,23 @@
 package fr.esgi.business;
 
+/**
+ * Enumeration décrivant le statut d'une Lettre selon les quatres états possibles pour celle-ci, indefini, absente du mot, présente mais au mauvais endroit, au bon endroit.
+ */
 public enum StatutLettre {
+    /**
+     * La valeur est VALIDE si le caractère est présent dans le mot à trouver mais n'est pas au bon endroit.
+     */
     VALIDE,
+    /**
+     * La valeur est TROUVE si le caractère est présent dans le mot à trouver et est au bon endroit.
+     */
     TROUVE,
+    /**
+     * La valeur est DEFAUT si aucun statut n'a encore été déterminé pour la Lettre.
+     */
     DEFAUT,
+    /**
+     * La valeur est ABSENTE si le caractère n'est pas présent dans le mot à trouver.
+     */
     ABSENTE
 }
